7999773027786c1ef3f976df25301b452d269119
asfian99 <mk.asfian@gmail.com>
2021-01-27 23:54:42 +0700

    convert to typescript & setup core dependency

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <div className="p-8 m-12 bg-gray-200 text-gray-700 rounded-lg">
+        <h1 className="text-center text-xl">Hello World</h1>
+      </div>
+    </>
+  );
+}
